02cf904e86609e9200009aa0875a6c6de9228363
JaviCandia <javi-candia@hotmail.com>
2020-12-06 12:26:08 -0600

    fixes

--- app/src/main/java/com/example/recepcindemensaje/MainActivity.java
+++ app/src/main/java/com/example/recepcindemensaje/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkSMSStatePermission();
         setContentView(R.layout.activity_main);
     }
 
